c97b4dc002865be1feb63b7ec4990d7bf290494d
Danny Johnsson <danny@voidpointer.com>
2024-04-26 14:11:13 +0200

    version 1.0.2.

--- src/nuget/publish_to_nugetserver.bat
+++ src/nuget/publish_to_nugetserver.bat
@@ -1,6 +1,6 @@
 
 set /p AuthCode=Enter nuget server auth code: 
 
-dotnet nuget push -s https://nuget2.voidpointer.se/v3/index.json -k %AuthCode% publish\VPBase.Client.1.0.1.nupkg
+dotnet nuget push -s https://nuget2.voidpointer.se/v3/index.json -k %AuthCode% publish\VPBase.Client.1.0.2.nupkg
 
 pause
